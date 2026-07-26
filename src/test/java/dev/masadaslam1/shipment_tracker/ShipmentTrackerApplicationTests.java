70d410cf321b5e7ad3fc37141b648570a4633175
Asad Aslam <asadaslam@live.com>
2026-07-26 16:05:31 +0200

    Spring boot app with dependencies

---
+++ src/test/java/dev/masadaslam1/shipment_tracker/ShipmentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.masadaslam1.shipment_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
